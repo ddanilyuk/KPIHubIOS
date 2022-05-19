86d6060cfb67294380906c2c242a738c39ca344f
denis.danyliuk <denis.danyliuk@requestum.com>
2022-05-19 16:04:08 +0300

    Initial Commit

---
+++ KPIHubIOS/KPIHubIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  KPIHubIOSApp.swift
+//  KPIHubIOS
+//
+//  Created by Denys Danyliuk on 19.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct KPIHubIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
