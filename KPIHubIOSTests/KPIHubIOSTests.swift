64640240726e08ac093f6319f5b54ec5d1d6a390
ddanilyuk <danis.danilyuk@gmail.com>
2023-10-02 23:59:41 +0300

    WIP
    WIP

--- KPIHubIOSTests/KPIHubIOSTests.swift
+++ KPIHubIOSTests/KPIHubIOSTests.swift
@@ -13,11 +13,8 @@ import SwiftUI
 
 class KPIHubIOSTests: XCTestCase {
     func testProfile() {
-//        let store = StoreOf<ProfileHome>(
-//            initialState: .init()
-//        ), reducer: ({
-//            ProfileHome()
-//        }),
+//        isRecording = true
+
         let store = StoreOf<ProfileHome>(
             initialState: ProfileHome.State(),
             reducer: {
@@ -27,11 +24,47 @@ class KPIHubIOSTests: XCTestCase {
                 $0.analyticsService = .none()
             }
         )
-        let profileView = NavigationStack {
-            ProfileHomeView(store: store)
+        let locales = [Locale(identifier: "en-US"), Locale(identifier: "uk-UA")]
+        let configs = [ViewImageConfig.iPhone8, ViewImageConfig.iPhone13, ViewImageConfig.iPhone13ProMax]
+        
+        createPairs(configs, locales).forEach { config, locale in
+            let profileView = NavigationStack {
+                ProfileHomeView(store: store)
+            }
+            .environment(\.locale, locale)
+            assertSnapshot(
+                of: profileView,
+                as: .image(perceptualPrecision: 0.99, layout: .device(config: config)),
+                named: "\(config.description)-\(locale.identifier)"
+            )
         }
-        isRecording = true
-//        let viewController =
-        assertSnapshot(of: profileView, as: .image(perceptualPrecision: 0.99, layout: .device(config: .iPhone13Pro)))
     }
+}
+
+extension ViewImageConfig {
+    var description: String {
+        switch self.size {
+        case ViewImageConfig.iPhone8.size:
+            return "iPhone8"
+        case ViewImageConfig.iPhone13.size:
+            return "iPhone13"
+        case ViewImageConfig.iPhone13ProMax.size:
+            return "iPhone13ProMax"
+        default:
+            return "no info"
+        }
+    }
+}
+
+
+func createPairs<T, U>(_ array1: [T], _ array2: [U]) -> [(T, U)] {
+    var pairs: [(T, U)] = []
+    
+    for item1 in array1 {
+        for item2 in array2 {
+            pairs.append((item1, item2))
+        }
+    }
+    
+    return pairs
 }
